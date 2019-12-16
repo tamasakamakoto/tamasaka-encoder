4cab20f94e3f806abc56c5c17de4c4e12f26400d
Tamasaka Makoto <>
2019-12-17 04:05:27 +0900

    add: encoder test

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "roots": [
+      "<rootDir>/src",
+      "<rootDir>/__tests__"
+    ],
+    "transform": {
+      "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+      "ts",
+      "tsx",
+      "js",
+      "jsx",
+      "json",
+      "node"
+    ],
+  }
+  
